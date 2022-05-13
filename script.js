7bf56f310e5c7b6809bccf9bbb91465f97160a1b
Luciene Freitas <luciene.freitas2015sjm@gmail.com>
2022-05-13 11:00:08 -0300

    refeito

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-const day18 = document.getElementById("day")
-const hour23 = document.getElementById("hour")
-const min28 = document.getElementById("min")
-const sec33 = document.getElementById("sec")
+const day18 = document.getElementById("day");
+const hour23 = document.getElementById("hour");
+const min28 = document.getElementById("min");
+const sec33 = document.getElementById("sec");
 
-const bithday = "14 agos 2022"
+const newYears = "1 Jan 2023";
 
 function cronometro(){
-  let actualDate = new Date();
-  let newYearDate = new Date(bithday)
-  let base = Math.floor((newYearDate-actualDate) / 1000)
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
 
-  let day = Math.floor(base / 3600 / 24)
-  let hour = Math.floor((base / 3600) % 24)
-  let min = Math.floor((base / 60) % 60)
-  let sec = Math.floor(base % 60)
-  
- 
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
 
-  day18.innerHTML = day
-  hour23.innerHTML = hours
-  min28.innerHTML = minutes
-  sec33.innerHTML = seconds
+  day18.innerHTML = days;
+  hour23.innerHTML = formatTime(hours);
+  min28.innerHTML = formatTime(minutes);
+  sec33.innerHTML = formatTime(seconds);
+
+};
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
 
 }
 
-cronometro()
+
+cronometro();
+
+setInterval(cronometro, 1000);
